44eba5221aed99b577cdf02987368c3420456bde
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-28 11:08:47 +0430

    category scchema

---
+++ server/Models/categoryMd.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const categorySchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required: [true, "لطفا نام را وارد کنید"],
+        trim:true
+    },
+    blogId:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Blog'
+    }]
+}<{timestamps:true})
+
+const Category=mongoose.model('Category',categorySchema)
+export default Category
