12952c9af96ae0df3f84712dd297c72fc32dfbb3
Barbodmsd <barbodmasoudi@gmail.com>
2024-07-29 13:44:41 +0430

    end deleted about

---
+++ server/Controllers/aboutCn.js
@@ -0,0 +1,70 @@
+import About from "../Models/aboutMd.js";
+import catchAsync from "../Utils/catchAsync.js";
+import HandleError from "../Utils/handleError.js";
+import returnData from "../Utils/returnData.js";
+import fs from "fs";
+import { __dirname } from "../app.js";
+
+export const createAbout = catchAsync(async (req, res, next) => {
+  const img = req?.file?.filename || "";
+  const newAbout = await About.create({ ...req.body, img });
+  return returnData(res, 201, {
+    status: "success",
+    data: { newAbout },
+  });
+});
+
+export const getAllAbout = catchAsync(async (req, res, next) => {
+  const allAbout = await About.find();
+  return returnData(res, 200, {
+    status: "success",
+    data: { allAbout },
+  });
+});
+
+export const getAboutById = catchAsync(async (req, res, next) => {
+  const about = await About.findById(req.params.id);
+  if (!about) {
+    return next(new HandleError("درباره ی موردنظر وجود ندارد"));
+  }
+  return returnData(res, 200, {
+    status: "success",
+    data: { about },
+  });
+});
+
+export const updateAbout = catchAsync(async (req, res, next) => {
+  const img = req?.file?.filename || "";
+  const oldAbout = await About.findById(req.params.is);
+  let updatedAbout;
+  if (img) {
+    updatedAbout = await About.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body, img },
+      { new: true, runVAlidators: true }
+    );
+    if (oldAbout.img) {
+      fs.unlinkSync(__dirname + "/Public/Image/" + oldAbout.img);
+    }
+  } else {
+    updatedAbout = await About.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runVAlidators: true,
+    });
+  }
+  return returnData(res, 200, {
+    status: "success",
+    data: { updatedAbout },
+  });
+});
+
+export const deleteAbout = catchAsync(async (req, res, next) => {
+  const deletedAbout = await About.findByIdAndDelete(req.params.id);
+  if (!deletedAbout) {
+    return next(new HandleError(" درباره ی  موردنظر وجود ندارد", 404));
+  }
+  return returnData(res, 200, {
+    status: "success",
+    data: { deletedAbout },
+  });
+});
--- server/Routes/about.js
+++ server/Routes/about.js
@@ -1,4 +1,18 @@
-import express from 'express'
-const aboutRoute=express.Router()
+import express from "express";
+import {deleteAbout, 
+  createAbout,
+  getAboutById,
+  getAllAbout,
+  updateAbout,
+} from "../Controllers/aboutCn.js";
+import upload from "../Utils/uploadFile.js";
+const aboutRoute = express.Router();
 
-export default aboutRoute 
+aboutRoute.route("/").post(upload.single("file"), createAbout).get(getAllAbout);
+aboutRoute
+  .route("/")
+  .patch(upload.single("file", updateAbout))
+  .get(getAboutById)
+  .delete(deleteAbout);
+
+export default aboutRoute;
