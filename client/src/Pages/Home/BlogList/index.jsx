1a36da12736e5c29cd05daf5fed70096294b9222
Barbodmsd <barbodmasoudi@gmail.com>
2024-09-10 00:10:45 +0430

    update

--- client/src/Pages/Home/BlogList/index.jsx
+++ client/src/Pages/Home/BlogList/index.jsx
@@ -1,7 +1,29 @@
 import { Box, Button, Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import fetchData from "../../../Utils/fetchData";
 
 export default function BlogList() {
+  const [value, setValue] = useState("");
+  const [blogs, setBlogs] = useState();
+  useEffect(() => {
+    (async () => {
+      const res = await fetchData(
+        value !=""?
+          `blog?filters[categoryId]=${
+            value === "فشن"
+              ? "66cccafc276bed12b1150cf6"
+              : value === "سفر"
+              ? "66cccae3276bed12b1150cf4"
+              : value === "طبیعت"
+              ? "66cccb04276bed12b1150cf8"
+              : value === "ماجراجویی" && "66cccb0b276bed12b1150cfa"
+          }`
+          :'blog'
+      );
+      setBlogs(res.blogs);
+      console.log(blogs);
+    })();
+  }, [value]);
   return (
     <>
       <Stack sx={{ my: "50px", p: "50px" }}>
@@ -12,13 +34,37 @@ export default function BlogList() {
             بیشترین علاقه مندی ها
           </Typography>
         </Box>
-        <Box sx={{textAlign:'right'}}>
-          <Stack direction='row' gap='10px'>
-            <Button sx={{ color: "mainTxt" }}>همه</Button>
-            <Button sx={{ color: "mainTxt" }}>همع</Button>
-            <Button sx={{ color: "mainTxt" }}>همه</Button>
-          </Stack>
-        </Box>
+        <Stack
+          direction='row'
+          gap='10px'
+          justifyContent='end'
+          sx={{ width: "100%", my: "20px" }}>
+          <Button
+            onClick={() => setValue("ماجراجویی")}
+            sx={{ color: value === "ماجراجویی" ? "mainTxt" : "white" }}>
+            <h2 sx={{ fontWeight: "bolder" }}>ماجراجویی</h2>
+          </Button>
+          <Button
+            onClick={() => setValue("سفر")}
+            sx={{ color: value === "سفر" ? "mainTxt" : "white" }}>
+            <h2 sx={{ fontWeight: "bolder" }}>سفر</h2>
+          </Button>
+          <Button
+            onClick={() => setValue("طبیعت")}
+            sx={{ color: value === "طبیعت" ? "mainTxt" : "white" }}>
+            <h2 sx={{ fontWeight: "bolder" }}>طبیعت</h2>
+          </Button>
+          <Button
+            onClick={() => setValue("فشن")}
+            sx={{ color: value === "فشن" ? "mainTxt" : "white" }}>
+            <h2 sx={{ fontWeight: "bolder" }}>فشن</h2>
+          </Button>
+          <Button
+            onClick={() => setValue("")}
+            sx={{ color: value === "" ? "mainTxt" : "white" }}>
+            <h2 sx={{ fontWeight: "bolder" }}>همه</h2>
+          </Button>
+        </Stack>
       </Stack>
     </>
   );
